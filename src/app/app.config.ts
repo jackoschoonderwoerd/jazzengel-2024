45bb64ffe69bbf851f70730baa0b13c1ec3adcac
jackoschoonderwoerd <34942572+jackoschoonderwoerd@users.noreply.github.com>
2024-06-08 20:32:40 +0200

    8 juni 2024 bis

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -9,25 +9,25 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getFunctions, provideFunctions } from '@angular/fire/functions';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { environment } from '../environments/environment';
+import { provideServiceWorker } from '@angular/service-worker';
 
 
 
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideRouter(routes),
-        provideAnimationsAsync(),
-        provideFirebaseApp(() => initializeApp(environment.firebase)),
-        // importProvidersFrom(
-        provideAuth(() => getAuth()),
-
-        provideFirestore(() => getFirestore()),
-
-        // importProvidersFrom(
-        //   provideFunctions(() => getFunctions())),
-
-        provideStorage(() => getStorage()), provideAnimationsAsync(),
-
-
-    ],
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    // importProvidersFrom(
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    // importProvidersFrom(
+    //   provideFunctions(() => getFunctions())),
+    provideStorage(() => getStorage()), provideAnimationsAsync(),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 };
